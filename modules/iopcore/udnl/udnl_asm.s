f787631f7d1d589a894785bdf088ce74695cc8da
AKuHAK <AKuHAK@gmail.com>
2019-01-30 00:52:47 +0200

    Revert "Replaced some asssembler registers"
    This reverts commit 67377e986461b91b85659146d1424959eb7b13fe.

--- modules/iopcore/udnl/udnl_asm.s
+++ modules/iopcore/udnl/udnl_asm.s
@@ -8,36 +8,36 @@
 func_00001440:
 	mfc0		$t0, $12
 	nop
-	la		$a4, func_00001440_main
+	la		$t4, func_00001440_main
 	lui		$at, 0xA000
-	or		$a4, $a4, $at
-	jr		$a4
+	or		$t4, $t4, $at
+	jr		$t4
 func_00001440_main:
 	mtc0		$zero, $12
 	nop
 	nop
-	lui		$a6, 0xbf80
-	lw		$a6, 0x1450($t6)
+	lui		$t6, 0xbf80
+	lw		$t6, 0x1450($t6)
 	nop
-	addiu		$a7, $zero, 0xfffe
-	and		$t1, $a6, $a7
+	addiu		$t7, $zero, 0xfffe
+	and		$t1, $t6, $t7
 	lui		$at, 0xbf80
 	sw		$t1, 0x1450($at)
 	lui		$at, 0xbf80
 	lw		$zero, 0x1450($at)
-	lui		$a7, 0xbf80
-	lw		$a7, 0x1578($t7)
+	lui		$t7, 0xbf80
+	lw		$t7, 0x1578($t7)
 	lui		$at, 0xbf80
 	sw		$zero, 0x1578($at)
 	lui		$at, 0xbf80
 	lw		$zero, 0x1578($at)
-	lui		$a5, 0xfffe
-	lw		$a5, 0x0130($t5)
+	lui		$t5, 0xfffe
+	lw		$t5, 0x0130($t5)
 	addiu		$t1, $zero, 0x0c04
 	lui		$at, 0xfffe
 	sw		$t1, 0x0130($at)
-	lui		$a4, 0x0001
-	mtc0		$a4, $12
+	lui		$t4, 0x0001
+	mtc0		$t4, $12
 	nop
 	nop
 	addiu		$t2, $zero, 0x0000
@@ -56,16 +56,16 @@ func_00001440_loop:
 	mtc0		$zero, $12
 	nop
 	lui		$at, 0xfffe
-	sw		$a5, 0x0130($at)
+	sw		$t5, 0x0130($at)
 	lui		$at, 0xfffe
 	lw		$zero, 0x0130($at)
 	nop
 	lui		$at, 0xbf80
-	sw		$a7, 0x1578($at)
+	sw		$t7, 0x1578($at)
 	lui		$at, 0xbf80
 	lw		$zero, 0x1578($at)
 	lui		$at, 0xbf80
-	sw		$a6, 0x1450($at)
+	sw		$t6, 0x1450($at)
 	lui		$at, 0xbf80
 	lw		$zero, 0x1450($at)
 	mtc0		$t0, $12
@@ -80,36 +80,36 @@ func_00001440_loop:
 #func_00001544:
 #	mfc0		$t0, $12
 #	nop
-#	la		$a4, func_00001544_main
+#	la		$t4, func_00001544_main
 #	lui		$at, 0xa000
-#	or		$a4, $a4, $at
-#	jr		$a4
+#	or		$t4, $t4, $at
+#	jr		$t4
 #func_00001544_main:
 #	mtc0		$zero, $12
 #	nop
 #	nop
-#	lui		$a6, 0xbf80
-#	lw		$a6, 0x1450($t6)
+#	lui		$t6, 0xbf80
+#	lw		$t6, 0x1450($t6)
 #	nop
-#	addiu		$a7, $zero, 0xfffe
-#	and		$t1, $a6, $a7
+#	addiu		$t7, $zero, 0xfffe
+#	and		$t1, $t6, $t7
 #	lui		$at, 0xbf80
 #	sw		$t1, 0x1450($at)
 #	lui		$at, 0xbf80
 #	lw		$zero, 0x1450($at)
-#	lui		$a7, 0xbf80
-#	lw		$a7, 0x1578($t7)
+#	lui		$t7, 0xbf80
+#	lw		$t7, 0x1578($t7)
 #	lui		$at, 0xbf80
 #	sw		$zero, 0x1578($at)
 #	lui		$at, 0xbf80
 #	lw		$zero, 0x1578($at)
-#	lui		$a5, 0xfffe
-#	lw		$a5, 0x0130($t5)
+#	lui		$t5, 0xfffe
+#	lw		$t5, 0x0130($t5)
 #	addiu		$t1, $zero, 0x00c4
 #	lui		$at, 0xfffe
 #	sw		$t1, 0x0130($at)
-#	lui		$a4, 0x0001
-#	mtc0		$a4, $12
+#	lui		$t4, 0x0001
+#	mtc0		$t4, $12
 #	nop
 #	nop
 #	addiu		$t2, $zero, 0x0000
@@ -128,16 +128,16 @@ func_00001440_loop:
 #	mtc0		$zero, $12
 #	nop
 #	lui		$at, 0xfffe
-#	sw		$a5, 0x0130($at)
+#	sw		$t5, 0x0130($at)
 #	lui		$at, 0xfffe
 #	lw		$zero, 0x0130($at)
 #	nop
 #	lui		$at, 0xbf80
-#	sw		$a7, 0x1578($at)
+#	sw		$t7, 0x1578($at)
 #	lui		$at, 0xbf80
 #	lw		$zero, 0x1578($at)
 #	lui		$at, 0xbf80
-#	sw		$a6, 0x1450($at)
+#	sw		$t6, 0x1450($at)
 #	lui		$at, 0xbf80
 #	lw		$zero, 0x1450($at)
 #	mtc0		$t0, $12
