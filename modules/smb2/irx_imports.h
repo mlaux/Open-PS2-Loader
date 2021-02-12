e36112432156230926aad04ff3429f87ed0b5368
Matthew Laux <matthew.laux@gmail.com>
2021-02-11 16:53:31 -0800

    update to compile with latest libsmb2 on master branch

--- modules/smb2/irx_imports.h
+++ modules/smb2/irx_imports.h
@@ -1,9 +1,12 @@
 #ifndef SMB2_IRX_IMPORTS_H
 #define SMB2_IRX_IMPORTS_H
 
+#include <stdio.h>
+#include <stdint.h>
+
 #include <loadcore.h>
 #include <ioman.h>
 #include <irx.h>
-#include <stdio.h>
+#include <smb2/libsmb2.h>
 
 #endif
--- modules/smb2/main.c
+++ modules/smb2/main.c
@@ -1,7 +1,25 @@
-#include <loadcore.h>
-#include <ioman.h>
-#include <irx.h>
-#include <stdio.h>
+#include "irx_imports.h"
+
+static void test_smb(void) {
+  // smb2 = smb2_init_context();
+  // if (smb2 == NULL) {
+  //         scr_setXY(10, y++);
+  //         scr_printf("init context failed.\n");
+  //         return 0;
+  // }
+  // scr_setXY(10, y++);
+  // scr_printf("init context SUCCEEDED.\n");
+  // sleep(10);
+  
+  // smb2_set_user(smb2, USER);
+  // smb2_set_password(smb2, PASSWORD);
+
+  // if (smb2_connect_share(smb2, SERVER, SHARE, USER) < 0) {
+  //         scr_setXY(10, y++);
+  //         scr_printf("smb2_connect_share failed. %s\n", smb2_get_error(smb2));
+  //         return 0;
+  // }
+}
 
 static int smb2_device_op_init(iop_device_t *device) {
   return 0;
@@ -40,11 +58,12 @@ iop_device_t smb2_device = {
 };
 
 int _start(int argc, char *argv[]) {
-  if (AddDrv(&smb2_device) == 0) {
-    printf("smb2 AddDrv success");
-    return MODULE_RESIDENT_END;
-  } else {
+  if (AddDrv(&smb2_device) != 0) {
     printf("smb2 AddDrv failure");
     return MODULE_NO_RESIDENT_END;
   }
+
+  printf("smb2 AddDrv success");
+  test_smb();
+  return MODULE_RESIDENT_END;
 }
