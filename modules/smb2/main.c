d6cc4832a8210d065441f2133927c977c40f8046
Matthew Laux <matthew.laux@gmail.com>
2021-02-11 22:29:46 -0800

    added working socket test code, still can't get smb to connect

--- modules/smb2/main.c
+++ modules/smb2/main.c
@@ -1,6 +1,28 @@
 #include "irx_imports.h"
 
 static void test_smb(void) {
+  // int sockfd;
+  // struct sockaddr_in addr;
+  // int err;
+  // socklen_t err_size = sizeof(err);
+
+  // sockfd = lwip_socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  // if (sockfd == -1) {
+  //   printf("socket() returned -1\n");
+  // }
+
+  // addr.sin_family = AF_INET;
+  // addr.sin_port = htons(445);
+  // ipaddr4_aton("172.16.1.136", (ip4_addr_t *) &addr.sin_addr.s_addr);
+
+  // err = lwip_connect(sockfd, (struct sockaddr *) &addr, sizeof addr);
+  // printf("connect() returned %d\n", err);
+
+  // printf("getsockopt() returned %d\n", getsockopt(sockfd, SOL_SOCKET, SO_ERROR,
+  //  (void *) &err, &err_size));
+  // printf("the socket error was %d\n", err);
+
+
   struct smb2_context *smb2;
 
   smb2 = smb2_init_context();
@@ -14,8 +36,10 @@ static void test_smb(void) {
   smb2_set_user(smb2, "guest");
   smb2_set_password(smb2, "");
 
-  if (smb2_connect_share(smb2, "172.16.0.110", "share1", "guest") < 0) {
+  if (smb2_connect_share(smb2, "172.16.1.136", "share1", "guest") < 0) {
     printf("failed to connect to share: %s\n", smb2_get_error(smb2));
+    printf("errno: %d\n", errno);
+    smb2_destroy_context(smb2);
     return;
   }
 
